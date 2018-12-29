f76988b4a15cc7337bf9ba38f63f2f168c72cf55
TMoechel <t.moechel@softcreation.de>
2018-12-29 23:38:37 +0100

    logic implemented

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var ds18b20 = require('ds18b20');
 ds18b20.sensors(function(err, ids) {
-  console.log(ids);
+  //console.log(ids);
+  ds18b20.temperature(ids[0], function(err, value){
+    console.log("Current Temperature is: ", value);
+  })
 });
