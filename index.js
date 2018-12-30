981d671234e1b65cf4cd063573c15818ed8a0d86
t.moechel <t.moechel@softcreation.de>
2018-12-30 19:00:27 +0100

    Controllers and Config added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var ds18b20 = require('ds18b20');
-ds18b20.sensors(function(err, ids) {
-  //console.log(ids);
-  ds18b20.temperature(ids[0], function(err, value){
-    console.log("Current Temperature is: ", value);
-  })
-});
+var config = require('./server/config/config');
+var app = require('./server/server');
+
+//var logger = require('./server/util/logger');
+
+app.listen(config.port);
+logger.log('listening on http://localhost:' + config.port);
