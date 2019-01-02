0a74460bd514be652b7e0d1cb6ef95c212972b27
t.moechel <t.moechel@softcreation.de>
2019-01-02 12:47:54 +0100

    cors added II

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var cors = require('cors')
 var api = require('./api/api');
 var config = require('./config/config');
 require('mongoose').connect(config.db.url);
 
 var app = express();
+app.use(cors);
 
 // setup the app middlware
 //require('./middleware/appMiddleware')(app);
