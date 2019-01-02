6e5c5479b9ccd0b352a10a91bd5c288951f23b29
t.moechel <t.moechel@softcreation.de>
2019-01-02 12:21:09 +0100

    live sensor data values

--- server/api/tempData/tempDataController.js
+++ server/api/tempData/tempDataController.js
@@ -29,8 +29,7 @@ function saveCurrentTempSensorValue() {
     if (err) console.log(err);
     else {
       ds18b20.temperature(ids[0], function(err, temp){
-        var itemp = 18;
-        var tempData = new TempData( {value : itemp, location : 'Lollschied', date : Date.now()});
+        var tempData = new TempData( {value : temp, location : 'Lollschied', date : Date.now()});
         tempData.save(function(err, tempData) {
           if (err) console.log(err);
         });
